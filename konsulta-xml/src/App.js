844b611c8eb8011a08a05e5134e6c3041e9854d5
janpaulo <janpaulobinaluyo@gmail.com>
2023-03-30 07:37:12 +0000

    setup new project

---
+++ konsulta-xml/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import NavItems from './shared/navItem';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled, useTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Dashboard from './../src/pages/home/dashboard'
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+function App() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+        <BrowserRouter>
+          <NavItems />
+
+          <CssBaseline />
+          <Container maxWidth="lg" style={{marginTop: '64px'}}>
+              <Routes>
+                <Route exact path="/"  element={ <Dashboard/> } />
+                {/* <Route exact path="/employees"  element={ <Employees/> } />
+                <Route exact path="/files" element={ <Files/> }  />
+                <Route exact path="/leaves" element={ <Leaves/> }  />
+                <Route exact path="/konsulta" element={ <Konsulta/> }  /> */}
+              </Routes>
+            </Container>
+        </BrowserRouter>
+
+   </Box>
+  );
+}
+
+export default App;
---
+++ konsulta-xml/src/pages/home/dashboard.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class dashboard extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            items : [
+                        
+                    ],
+        };
+      }
+      render(){
+        return (
+            <>
+             sasaaaaaaaaaaaaa
+
+
+
+             saaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+
+             sa
+             sa
+             sasaaaaaaaaaaaaasa
+             s
+             as
+             a
+             sa
+             s
+             SaveAsTwoToneIconas
+             a
+             s
+
+            </>
+      )
+    }
+  
+  }
+
+    
+export default dashboard;
