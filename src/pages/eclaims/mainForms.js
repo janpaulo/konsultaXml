7bd19fc7df90c0cffc73324a8650590d3396a32e
janpaulo <janpaulobinaluyo@gmail.com>
2024-01-13 06:39:52 +0000

    inatial3 registration

--- src/pages/eclaims/mainForms.js
+++ src/pages/eclaims/mainForms.js
@@ -120,7 +120,7 @@ class mainForms extends React.Component {
           </Tabs>
 
           <CustomTabPanel value={this.state.value} index={0}>
-            <Forms2
+            <Forms
               handleClick={this.handleSubmit}
               onchange={this.handleInputChange}
             />
